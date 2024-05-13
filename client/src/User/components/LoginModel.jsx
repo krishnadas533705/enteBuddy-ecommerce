3d7a3a178a1951b0d07be07004573620b158de55
Krishnadas K S <krishnadas533706@gmail.com>
2024-05-13 13:49:17 +0530

    shiprocket and razorpay added

--- client/src/User/components/LoginModel.jsx
+++ client/src/User/components/LoginModel.jsx
@@ -1,144 +1,145 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import { IoCloseCircleOutline } from "react-icons/io5"
-import { useRef } from 'react';
-import { BsFillShieldLockFill,BsTelephoneFill } from "react-icons/bs";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { useRef } from "react";
+import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs";
 import { CgSpinner } from "react-icons/cg";
-import { useState ,useContext} from "react";
-import OtpInput from "otp-input-react"
+import { useState, useContext } from "react";
+import OtpInput from "otp-input-react";
 import PhoneInput from "react-phone-input-2";
-import 'react-phone-input-2/lib/style.css';
+import "react-phone-input-2/lib/style.css";
 
 import { auth } from "../utils/firebase";
-import { RecaptchaVerifier,signInWithPhoneNumber } from "firebase/auth";
-import { Toaster,toast } from "react-hot-toast";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { Toaster, toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import peakpx from '../img/peakpx.jpg'
+import peakpx from "../img/peakpx.jpg";
 import { userContext } from "../contexts/UserContext";
 import { CartContext } from "../contexts/CartContext";
 
-const LoginModel = ({onClose}) => { 
-
-  const [otp,setOtp]=useState("")
-  const [ph,setPh] = useState("")
-  const [loading,setLoading]=useState("")
-  const [showOtp,setShowOtp]=useState("")
-  const [user,setUser] =useState(null)
+const LoginModel = ({ onClose }) => {
+  const [otp, setOtp] = useState("");
+  const [ph, setPh] = useState("");
+  const [loading, setLoading] = useState("");
+  const [showOtp, setShowOtp] = useState("");
+  const [user, setUser] = useState(null);
   const { userId, setUserId } = useContext(userContext);
   const { setCart, fetchCart } = useContext(CartContext);
-  
-  const navigate = useNavigate();  //intialize the navigate function 
 
-  function onCaptchVerify(){
-    if(!window.RecaptchaVerifier){
-      window.RecaptchaVerifier=new RecaptchaVerifier (
-        auth , 'recaptcha-container', 
+  const navigate = useNavigate(); //intialize the navigate function
+
+  function onCaptchVerify() {
+    if (!window.RecaptchaVerifier) {
+      window.RecaptchaVerifier = new RecaptchaVerifier(
+        auth,
+        "recaptcha-container",
         {
-          size : 'invisible',
-          callback : (response) =>{
+          size: "invisible",
+          callback: (response) => {
             onSignup();
           },
-          "expired-callback" : ()=> {},
+          "expired-callback": () => {},
         }
-      )
+      );
     }
-  } 
-  async function onSignup(){
+  }
+  async function onSignup() {
     setLoading(true);
     await onCaptchVerify();
 
     const appVerifier = window.RecaptchaVerifier;
-    const formatPh= "+"+ ph ;
-    signInWithPhoneNumber(auth,formatPh,appVerifier)
-    .then((confirmationResult)=>{
-      window.confirmationResult = confirmationResult ;
-      setLoading(false);
-      setShowOtp(true);
-      toast.success("otp sent successfully")
-    })
-    .catch((err)=>{
-      console.log(err);
-      setLoading(false);
-    })
-  } 
-  function onOTPVerify(){
+    const formatPh = "+" + ph;
+    signInWithPhoneNumber(auth, formatPh, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        setLoading(false);
+        setShowOtp(true);
+        toast.success("otp sent successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
+  function onOTPVerify() {
     setLoading(true);
     window.confirmationResult
-    .confirm(otp).then(async (res)=>{
-      console.log(res);
-      setUser(res.user)
-      setLoading(false);
-      console.log(ph)
-      if(res.user){
-        await userSignin(); //call the user signin after otp verification 
-      }
-    }).catch((err)=>{
-      console.log(err);
-      setLoading(false);
-    })
+      .confirm(otp)
+      .then(async (res) => {
+        console.log("firebase response : ", res);
+        setUser(res.user);
+        setLoading(false);
+        console.log("phone number : ", ph);
+        if (res.user) {
+          await userSignin(); //call the user signin after otp verification
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }
 
-  async function userSignin(){
+  async function userSignin() {
     try {
-      const response =await fetch ("/api/auth/userAuth",{
-        method : "POST",
-        headers : {
-          "content-type" : "application/json",
-        }, 
-        body : JSON.stringify({value: ph})
-      } );
-      if(response.ok){ 
-        
-        console.log("log success")
+      const response = await fetch("/api/auth/userAuth", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ value: ph }),
+      });
+      if (response.ok) {
+        console.log("log success");
         const userData = await response.json();
-        console.log(userData)
-        localStorage.setItem('enteBuddyUser',userData._id);
-        console.log(localStorage.getItem('entebuddyUser'));
-        
+        console.log(userData);
+        localStorage.setItem("enteBuddyUser", userData._id);
+        console.log(localStorage.getItem("entebuddyUser"));
 
         setUser(userData._id);
-        setUserId(userData._id)
-        
-        await fetchCart(userData._id); 
-        toast.success("user logged in")
-        
-        
-        
-        await new Promise(resolve=>
-          setTimeout(resolve,2000)
-        ) 
-        onClose()
+        setUserId(userData._id);
 
-      } else 
-      {
-        alert("failed to log in ")
-      } 
-    }
-      catch(err){
-        console.log("error signing in : ",err)
+        await fetchCart(userData._id);
+        toast.success("user logged in");
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        onClose();
+      } else {
+        alert("failed to log in ");
       }
-     
+    } catch (err) {
+      console.log("error signing in : ", err);
+    }
   }
 
-  const modalRef=useRef();
-  
-  const closeModal=(e)=>{
-   if(modalRef.current===e.target){
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
       onClose();
-   }
-  }
+    }
+  };
   return (
-    <div ref={modalRef} onClick= {closeModal} className="z-10 text-white fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center">
-        <div  className='flex flex-col '>
-        <div id="recaptcha-container" ></div>
-        <Toaster toastOptions={{ duration: 4000  }} />
-            <button onClick={onClose} className='place-self-end'><IoCloseCircleOutline/></button>
-            <div className={`border border-blue-100 py-10 px-10  bg-hero bg-cover rounded-xl flex flex-col gap-5 mx-3 `}>
-                
-                <h1  className="text-3xl font-jaro text-yellow-300  text-center ">Login through otp</h1>
+    <div
+      ref={modalRef}
+      onClick={closeModal}
+      className="z-10 text-white fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center"
+    >
+      <div className="flex flex-col ">
+        <div id="recaptcha-container"></div>
+        <Toaster toastOptions={{ duration: 4000 }} />
+        <button onClick={onClose} className="place-self-end">
+          <IoCloseCircleOutline />
+        </button>
+        <div
+          className={`border border-blue-100 py-10 px-10  bg-hero bg-cover rounded-xl flex flex-col gap-5 mx-3 `}
+        >
+          <h1 className="text-3xl font-jaro text-yellow-300  text-center ">
+            Login through otp
+          </h1>
 
-                <hr />
-                {showOtp ? (
+          <hr />
+          {showOtp ? (
             <>
               <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
                 <BsFillShieldLockFill size={30} />
@@ -179,7 +180,12 @@ const LoginModel = ({onClose}) => {
               >
                 Verify your phone number
               </label>
-              <PhoneInput country={"in" } value={ph} onChange={setPh} className="text-black" />
+              <PhoneInput
+                country={"in"}
+                value={ph}
+                onChange={setPh}
+                className="text-black"
+              />
               <button
                 onClick={onSignup}
                 className="bg-[#54d8b5] w-full flex gap-1 items-center justify-center py-2.5 text-white font-figtree rounded"
@@ -191,12 +197,10 @@ const LoginModel = ({onClose}) => {
               </button>
             </>
           )}
-            </div>
-    
         </div>
-    
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginModel;   
+export default LoginModel;
--- server/controllers/payments.controller.js
+++ server/controllers/payments.controller.js
@@ -2,22 +2,20 @@ import product from "../models/product.model.js";
 import { instance } from "../index.js";
 import crypto from "crypto";
 import { errorHandler } from "../utils/error.js";
+import order from "../models/order.model.js";
 
 export const createPayment = async (req, res, next) => {
   try {
-    const productId = req.body.productId;
-    const productDetails = await product.findById(productId);
-    const amount =
-      productDetails.price -
-      (productDetails.price * productDetails.discount) / 100;
+    const amount = req.body.amount;
     console.log("Amount : ", amount);
     const options = {
       amount: amount * 100,
       currency: "INR",
+      notes: { userId: req.user._id },
     };
 
     const order = await instance.orders.create(options);
-    order.amount = amount;
+    console.log("order razorpay : ", order);
     res.status(200).json({
       order,
       key_id: process.env.RAZORPAY_ID,
@@ -30,22 +28,45 @@ export const createPayment = async (req, res, next) => {
 
 export const verifyPayment = async (req, res, next) => {
   try {
+    console.log("payment verifying....");
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
       req.body;
-    const body = razorpay_order_id + "|" + razorpay_payment_id;
-
+    console.log("razorpay body : ", req.body);
+    console.log("secret : ", process.env.RAZORPAY_SECRET_KEY);
     const expectedSignature = crypto
       .createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
-      .update(body.toString())
+      .update(`${razorpay_order_id}|${razorpay_payment_id}`)
       .digest("hex");
+    console.log(expectedSignature, " : ", razorpay_signature);
     if (expectedSignature == razorpay_signature) {
       console.log("payment success");
       res.status(200).json("payment success");
-      //database
-      // redirect to front end or send success response
     } else {
-      return next(errorHandler(400, "payment verification failed"));
+      console.log("payment failed");
     }
+  } catch (err) {
+    next(err);
+  }
+};
+
+///refund payment
+export const refundPayment = async (req, res, next) => {
+  try {
+    const paymentId = req.body.paymentId;
+    const response = await instance.payments.refund(paymentId, {
+      speed: "optimum",
+    });
+    console.log("response : ",response)
+    if (response.id) {
+      await order.updateOne(
+        { _id: req.user._id },
+        { $set: { "orders.$[element].refundId": response.id } },
+        { arrayFilters: [{ "element.paymentId": paymentId }] }
+      );
+    } else {
+      throw new Error(response.description);
+    }
+    res.status(200).send("Refund initiated");
   } catch (err) {
     next(err);
   }
