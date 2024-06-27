5df56398d518864518ed6fceda5a2a300196da4e
Krishnadas K S <krishnadas533706@gmail.com>
2024-06-27 17:52:11 +0530

    banner removed

--- client/src/User/components/Banner.jsx
+++ client/src/User/components/Banner.jsx
@@ -4,7 +4,7 @@ import banner from '../img/banner.webp'
 const Banner = () => {
   return (
     <div className='w-full p-4 lg:px-16 lg:pt-7  '>
-        <img src={banner} alt='img' className='rounded-3xl lg:max-h-[400px] w-full object-fit'/>
+        {/* <img src={banner} alt='img' className='rounded-3xl lg:max-h-[400px] w-full object-fit'/> */}
 
     </div>
   )
