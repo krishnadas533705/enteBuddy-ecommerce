4e57be7ae2e2867b7c5fae9e48fea39b92399cc6
Krishnadas K S <krishnadas533706@gmail.com>
2024-06-27 17:16:12 +0530

    fileUpload path changed

--- server/utils/fileUpload.js
+++ server/utils/fileUpload.js
@@ -5,11 +5,11 @@ import { __dirname } from "../app.js";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     if (file.fieldname == "primaryImage") {
-      cb(null,path.join(__dirname,'/Public/primaryImages'));
+      cb(null,path.join(__dirname,'/Public/uploads/primaryImages'));
     } else if (file.fieldname == "secondaryImages") {
-      cb(null, path.join(__dirname,'/Public/secondaryImages'));
+      cb(null, path.join(__dirname,'/Public/uploads/secondaryImages'));
     } else if (file.fieldname == "bannerImage") {
-      cb(null, path.join(__dirname,'/Public/bannerImages'));
+      cb(null, path.join(__dirname,'/Public/uploads/bannerImages'));
       
     } else {
       cb(new Error("Unexpected field"));
